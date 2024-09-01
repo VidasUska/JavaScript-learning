73fa07706d5ebf5dcdedd08d92b06393c4c780a5
Vidas Uska <vidas.uska@gmail.com>
2024-09-01 22:26:06 +0300

    branch feature commit

--- index.js
+++ index.js
@@ -264,10 +264,10 @@
 
 //*********** 20- Exercise 6- Price Range Object ********//
 
-// let priceRange = [
-//   { label: "$", tooltip: "Inexpensive", minPerPerson: 0, maxPerPerson: 10 },
-//   { label: "$$", tooltip: "Moderate", minPerPerson: 11, maxPerPerson: 20 },
-//   { label: "$$$", tooltip: "Expensive", minPerPerson: 21, maxPerPerson: 50 },
-// ];
+let priceRange = [
+  { label: "$", tooltip: "Inexpensive", minPerPerson: 0, maxPerPerson: 10 },
+  { label: "$$", tooltip: "Moderate", minPerPerson: 11, maxPerPerson: 20 },
+  { label: "$$$", tooltip: "Expensive", minPerPerson: 21, maxPerPerson: 50 },
+];
 
-// let restorant = [{ averagePerPerson: 5 }];
+let restorant = [{ averagePerPerson: 5 }];
